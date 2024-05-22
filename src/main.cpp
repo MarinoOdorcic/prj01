ace70320aededaddece15f22ca2674f406932d1d
MarinoOdorcic <modorcic@student.uniri.hr>
2024-05-22 06:03:09 +0200

    it2

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,56 @@
-#include <fmt/core.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
-#include <nlohmann/json.hpp>
-#include <fstream>
 
 #include <vector>
 #include <memory>
 #include <algorithm>
 #include "element.h"
-
-#include "pipe.h"
-#include "local.h"
 #include "json.h"
 
 double getPressure(std::vector<std::unique_ptr<Element>> &vec) {
     auto it = std::find_if(vec.begin(), vec.end(), [](const std::unique_ptr<Element>& obj) {
         return obj->getID()[0] == "NONE";});
-    rotate(vec.begin(), it, it + 1);
-//        fmt::print("\n{}", "rotate");
-    return vec[std::distance(vec.begin(), it)]->getProperties()[0];
+    return (*it)->getProperties()[0];
 }
 
 double getFlow(std::vector<std::unique_ptr<Element>> &vec) {
     auto it = std::find_if(vec.begin(), vec.end(), [](const std::unique_ptr<Element>& obj) {
         return obj->getID()[2] == "NONE";});
-    rotate(it, it + 1, vec.end());
-//        fmt::print("\n{}", "rotate");
-    return vec[std::distance(vec.begin(), it)]->getProperties()[0];
+    return (*it)->getProperties()[0];
 }
 
-Element& start(std::vector<std::unique_ptr<Element>> &vec){
+std::string start(std::vector<std::unique_ptr<Element>> &vec) {
     auto it = std::find_if(vec.begin(), vec.end(), [](const std::unique_ptr<Element>& obj) {
         return obj->getID()[0] == "NONE";});
-    return **it;
+    return (*it)->getID()[2];
 }
 
-void first(std::vector<std::unique_ptr<Element>>& vec){
-    if (vec[0]->getID()[0] != "NONE"){
-        auto it = std::find_if(vec.begin(), vec.end(), [](const std::unique_ptr<Element>& obj) {
-            return obj->getID()[0] == "NONE";});
-        rotate(vec.begin(), it, it + 1);
-//        fmt::print("\n{}", "rotate");
-    }
+std::unique_ptr<Element>& downstream(std::vector<std::unique_ptr<Element>> &vec, std::string id){
+    auto it = std::find_if(vec.begin(), vec.end(), [&id](const std::unique_ptr<Element> &obj) {
+        return obj->getID()[1] == id;});
+    return vec[std::distance(vec.begin(), it)];
 }
 
-void last(std::vector<std::unique_ptr<Element>>& vec){
-    if (vec[vec.size()-1]->getID()[2] != "NONE"){
-        auto it = std::find_if(vec.begin(), vec.end(), [](const std::unique_ptr<Element>& obj) {
-            return obj->getID()[2] == "NONE";});
-        rotate(it, it + 1, vec.end());
-//        fmt::print("\n{}", "rotate");
-    }
-}
+void solve(std::vector<std::unique_ptr<Element>>& vec) {
+    double Q = getFlow(vec);
+    double H = getPressure(vec);
+    double dH = 0;
+    std::string id = start(vec);
 
-Element& downstream(Element& el, std::vector<std::unique_ptr<Element>> &vec){
-    auto it = std::find_if(vec.begin(), vec.end(), [&vec, &el](const std::unique_ptr<Element> &obj) {
-        return el.getID()[2] == obj->getID()[1];});
-    return **it;
-}
-
-void sort(std::vector<std::unique_ptr<Element>>& vec){
-    for (size_t i = 0; i < vec.size()-1; ++i) {
-        if (vec[i]->getID()[2] != vec[i + 1]->getID()[1]) {
-            auto it = std::find_if(vec.begin() + i, vec.end(), [&vec, &i](const std::unique_ptr<Element> &obj) {
-                return vec[i]->getID()[2] == obj->getID()[1];});
-            rotate(vec.begin() + i + 1, it, it + 1);
-//            fmt::print("\n{}", "rotate");
+    for (int i = 1; i < vec.size()-1; ++i){
+        std::unique_ptr<Element>& element = downstream(vec,id);
+        if (element->getID()[2] == "NONE"){break;}
+        if (i==1){
+            H += element->getProperties()[1];
         }
-    }
-}
-
-void calc(std::vector<std::unique_ptr<Element>>& vec){
-    for (size_t i = 1; i < vec.size()-1; ++i){
-        auto Q = getFlow(vec);
-        vec[i]->calcPressureDrop(Q);
-    }
-}
-
-//void solve(std::vector<std::unique_ptr<Element>>& vec){
-//    sort(vec);
-//    calc(vec);
-//    auto H = getPressure(vec);
-//    auto &first = start(vec);
-//    auto &next = downstream(first,vec);
-//    auto Z = first.getProperties()[0];
-//    auto dH = downstream(first,vec).getPressureDrop();
-//
-//    next.calcPiezometricHead(Z + H, dH);
-//
-////    vec[1]->calcPiezometricHead(firstElement(vec).getProperties()[0]+ H,
-////                                vec[1]->getPressureDrop());
-//    for (size_t i = 2; i < vec.size() - 1; ++i){
-////        if (i != 2){
-////            double dZ = vec[el]->
-////        } else {
-////            double dZ = - getPressure(vec);
-////        }
-//        if (vec[i]->getType() == PIPE){
-//            vec[i]->calcPiezometricHead(vec[i-2]->getProperties()[5]-vec[i-1]->getPressureDrop(),
-//                                        vec[i]->getPressureDrop());
-//        }
-//    }
-//}
-
-void solve(std::vector<std::unique_ptr<Element>>& vec){
-    sort(vec);
-    calc(vec);
-    vec[1]->calcPiezometricHead(vec[0]->getProperties()[0]+vec[1]->getProperties()[1],
-                                vec[1]->getPressureDrop());
-    for (size_t i = 2; i < vec.size() - 1; ++i){
-        if (vec[i]->getType() == PIPE){
-            vec[i]->calcPiezometricHead(vec[i-2]->getProperties()[5]-vec[i-1]->getPressureDrop(),
-                                        vec[i]->getPressureDrop());
+        element->calcPressureDrop(Q);
+        dH += element->getPressureDrop();
+        if (element->getType()==PIPE){
+            element->calcPiezometricHead(H, dH);
+            dH = 0;
+            H = element->getProperties()[5];
         }
+        id = element->getID()[2];
     }
 }
 
@@ -156,7 +94,3 @@ int main()
     fmt::print("\n{}\n", std::string(70, '-'));
     return 0;
 }
-
-
-
-
